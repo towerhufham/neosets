027dc91334f8d35a25e8736a305936d1e7defaad
towerhufham <32599109+towerhufham@users.noreply.github.com>
2026-03-05 11:14:40 -0500

    First page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Neosets</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="w-full text-center text-xl">Welcome, {{$name}}.</h1>
+  <h2 class="w-full text-center text-md text-gray-300 italic">{{time()}}</h2>
+</body>
+</html>
