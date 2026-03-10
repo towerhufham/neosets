3efdf0541b87f328474e65a8fd6a45c1d9e840e6
towerhufham <32599109+towerhufham@users.noreply.github.com>
2026-03-10 14:08:20 -0400

    Login view & layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,17 @@
-<x-layouts.app title="Register">
-  <h1 class="w-full text-center text-xl">Let's get you all registered!!!!!!</h1>
+<x-layouts.app title="Login">
+  <h1 class="w-full text-center text-xl">Let's get you all logged in bruv!!!!!!</h1>
   <h2 class="w-full text-center text-md text-gray-300 italic">{{time()}}</h2>
 
-  <form class="max-w-4xl bg-amber-100 rounded-3xl p-7 mx-auto" action="/register" method="POST">
+  <form class="max-w-4xl bg-amber-100 rounded-3xl p-7 mx-auto" action="/login" method="POST">
     @csrf
     <div class="w-full justify-between flex">
       <div class="flex flex-col items-center justify-center mb-4">
         <label for="username">Username:</label>
         <input class="bg-white p-1 rounded-md border border-black" name="name" type="text"/>
       </div>
-      <div class="flex flex-col items-center justify-center mb-4">
-        <label for="username">Email:</label>
-        <input class="bg-white p-1 rounded-md border border-black" name="email" type="email"/>
-      </div>
       <div class="flex flex-col items-center justify-center">
         <label for="password">Password:</label>
         <input class="bg-white p-1 rounded-md border border-black" name="password" type="password"/>
-      </div>
-      <div class="flex flex-col items-center justify-center">
-        <label for="password_confirmation">Confirm Password:</label>
-        <input class="bg-white p-1 rounded-md border border-black" name="password_confirmation" type="password"/>
       </div>
     </div>
     <button class="bg-amber-400 rounded-lg shadow-lg p-2 cursor-pointer" type="submit">Submit</button>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Neosets</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
+<x-layouts.app title="Home">
   <h1 class="w-full text-center text-xl">Welcome, {{$name}}.</h1>
   <h2 class="w-full text-center text-md text-gray-300 italic">{{time()}}</h2>
-</body>
-</html>
+</x-layouts.app>
