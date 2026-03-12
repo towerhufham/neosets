d27cabef4c06c9d18ccef2e0b92db97ee56d9917
Tower Hufham <towerhufham@yahoo.com>
2026-03-12 14:00:56 -0400

    Home shows item grid

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,18 @@
   <h1 class="w-full text-center text-xl">Welcome, {{ Auth::user()->name }}.</h1>
   <h2 class="w-full text-center text-md text-gray-300 italic">{{time()}}</h2>
   <livewire:counter/>
+  <div class="max-w-4xl mx-auto">
+    <div class="grid grid-cols-6 gap-4">
+      @foreach ($items as $item)
+        <div class="flex flex-col justify-center items-center">
+          <img 
+          class="border border-black"
+          src="https://images.neopets.com/items/{{$item->img_name}}.gif" 
+          alt="{{$item->name}}"/>
+          <p class="font-bold font-serif text-center">{{$item->name}}</p>
+          <p class="text-gray-500">{{number_format($item->base_value)}} NP</p>
+        </div>
+      @endforeach
+    </div>
+  </div>
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Item;
 
 Route::get('/', function () {
     return view('pages.welcome');
 });
 
 Route::get('/home', function () {
-    return view('pages.home');
+    return view('pages.home', ['items' => Item::all()]);
 })->middleware(('auth'));
