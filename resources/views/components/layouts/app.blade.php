b0701635bef3647de923f391483b727ccad359a1
towerhufham <32599109+towerhufham@users.noreply.github.com>
2026-03-17 11:35:52 -0400

    Items are livewire components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Neosets {{ $title ? "— $title" : ''}}</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @livewireStyles
 </head>
 <body>
   @auth
@@ -22,5 +23,6 @@
   <main>
     {{ $slot }}
   </main>
+  @livewireScripts
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,27 +5,7 @@
     <h2 class="text-xl font-bold text-center">For Sale:</h2>
     <div class="grid grid-cols-6 gap-4">
       @foreach ($offers as $offer)
-        <div class="flex flex-col justify-center items-center">
-          <img 
-          class="border border-blue-500"
-          src="https://images.neopets.com/items/{{$offer->item->img_name}}.gif" 
-          alt="{{$offer->item->name}}"/>
-          <p class="font-bold font-serif text-center">{{$offer->item->name}}</p>
-          <p class="text-gray-500">{{number_format($offer->price)}} NP</p>
-          @if ($offer->item->rarity === 100)
-            <p class="text-amber-600 font-bold">(MEGA RARE)</p>
-          @elseif ($offer->item->rarity === 99)
-            <p class="text-green-600 font-bold">(ultra rare)</p>
-          @elseif ($offer->item->rarity >= 95)
-            <p class="text-green-600 font-bold">(super rare)</p>
-          @elseif ($offer->item->rarity >= 90)
-            <p class="text-green-600 font-bold">(very rare)</p>
-          @elseif ($offer->item->rarity >= 85)
-            <p class="text-green-600 font-bold">(rare)</p>
-          @elseif ($offer->item->rarity >= 75)
-            <p class="text-green-600 font-bold">(uncommon)</p>
-          @endif
-        </div>
+        <livewire:item-for-sale :item="$offer->item" :price="$offer->price"/>
       @endforeach
     </div>
   </div>
